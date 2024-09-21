a426ebd11a6738176d9b92c60ba648deb2bd6630
ganeshapple <chganeshprasad45@gmail.com>
2024-09-21 16:58:26 +0530

    commit

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-fs.readFile('http1.js', 'utf-8', (err, data) => {
+fs.readFile('test.txt', 'utf-8', (err, data) => {
     if (err) {
         console.log(err); 
     } else {
